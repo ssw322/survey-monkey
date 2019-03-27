efc3ddbba58707784348011ae70c6c52c062dd85
mfreema1 <mfreema1@stevens.edu>
2019-03-27 13:42:33 -0400

    Provide view constants

---
+++ app/src/main/java/com/ssw322/project/surveylemur/form/question/Constants.java
@@ -0,0 +1,9 @@
+package com.ssw322.project.surveylemur.form.question;
+
+public class Constants {
+
+    public static final int VIEW_TYPE_MULTIPLE_CHOICE = 0;
+    public static final int VIEW_TYPE_MULTIPLE_ANSWER = 1;
+    public static final int VIEW_TYPE_SHORT_ANSWER = 2;
+    public static final int VIEW_TYPE_ESSAY = 3;
+}
