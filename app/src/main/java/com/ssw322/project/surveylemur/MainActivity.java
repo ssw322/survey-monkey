b4dacfa517d208cde7a3375c5ff11e3cc680de2b
Mike Ryan <mryan6@stevens.edu>
2019-04-19 18:54:56 -0400

    Finds code in database
    If a user enters a code that is in the database, the app will succesfully change to the View Form activity. Currently having trouble pulling data from Firebase since I think we need to deserealize it manually.

--- app/src/main/java/com/ssw322/project/surveylemur/MainActivity.java
+++ app/src/main/java/com/ssw322/project/surveylemur/MainActivity.java
@@ -11,8 +11,15 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ssw322.project.surveylemur.create.CreateSurveyActivity;
 import com.ssw322.project.surveylemur.create.CreateTestActivity;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,12 +36,32 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText editText = findViewById(R.id.code_field);
                 String codeEntered = editText.getText().toString();
-
+                FirebaseDatabase db = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = db.getReference(codeEntered);
                 if(codeEntered.trim().length() != 4) {
                     //let the user know that they must input something
                     Toast.makeText(getBaseContext(), "You must enter a valid 4-digit code", Toast.LENGTH_SHORT).show();
+                } else {
+                    myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if(dataSnapshot.exists()) {//if the code exists in the database
+                                Intent intent = new Intent(MainActivity.this, ViewFormActivity.class);
+                                intent.putExtra("code", codeEntered); //we need to pass the entered code from this activity to ViewFormActivity so we can pull data from the database
+                                startActivity(intent);
+                            }
+                            else {
+                                Toast.makeText(getBaseContext(), "No test found with entered code", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
                 }
-                //make a call to the db somehow and show an alert dialogue if successful
             }
         });
 
